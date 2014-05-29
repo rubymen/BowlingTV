221ebd39e8d849bcdf15849c2989714cdd9e4b4c
Rémi Delhaye <remi.delhaye@epsi.fr>
2014-05-29 15:26:49 +0200

    Add WebService (thanks @nicodvk) and comments on code.

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+// On initialise l'application Express, les routes, et le serveur http
+
 var express = require('express');
 var routes = require('./routes');
 var tv = require('./routes/tv');
 var http = require('http');
 var path = require('path');
 
+// On crée l'instance de l'application Express
+
 var app = express();
 
-// all environments
+// On définie l'environement de l'application Express
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,403 +24,102 @@ app.use(app.router);
 app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+// On activent l'affichage des erreurs dans la console pour le mode "development"
+
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// On définie les routes de l'application
+
 app.get('/', routes.index);
 app.get('/:tv', tv.index);
+
+// On crée et lance le serveur HTTP
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+// On attache "socket.io" au serveur HTTP
+
 var io = require('socket.io').listen(server);
 
-var games =
-{
-  'games':
-  [{
-    'title': 'Partie 11234',
-    'id': 1,
-    'piste': 1,
-    'team': [{
-      'name': 'Rémi Delhaye',
-      'position': 1,
-      'coups': [{
-        'numero': 1,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 5
-        },
-        {
-          'numero': 2,
-          'quilles': 3
-        }]
-      },
-      {
-        'numero': 2,
-        'strike': true,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 'X'
-        },
-        {
-          'numero': 2,
-          'quilles': '/'
-        }]
-      },
-      {
-        'numero': 3,
-        'strike': true,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 'X'
-        },
-        {
-          'numero': 2,
-          'quilles': '/'
-        }]
-      },
-      {
-        'numero': 4,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 6
-        },
-        {
-          'numero': 2,
-          'quilles': 2
-        }]
-      },
-      {
-        'numero': 5,
-        'strike': false,
-        'spare': true,
-        'lances': [{
-          'numero': 1,
-          'quilles': 6
-        },
-        {
-          'numero': 2,
-          'quilles': '/'
-        }]
-      },
-      {
-        'numero': 6,
-        'strike': true,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 'X'
-        },
-        {
-          'numero': 2,
-          'quilles': '/'
-        }]
-      },
-      {
-        'numero': 7,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 1
-        },
-        {
-          'numero': 2,
-          'quilles': 7
-        }]
-      },
-      {
-        'numero': 8,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 2
-        },
-        {
-          'numero': 2,
-          'quilles': 2
-        }]
-      },
-      {
-        'numero': 9,
-        'strike': false,
-        'spare': true,
-        'lances': [{
-          'numero': 1,
-          'quilles': 4
-        },
-        {
-          'numero': 2,
-          'quilles': '/'
-        }]
-      },
-      {
-        'numero': 10,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 2
-        },
-        {
-          'numero': 2,
-          'quilles': 3
-        }]
-      }]
-    },
-    {
-      'name': 'Baptiste Lecocq',
-      'position': 2,
-      'coups': [{
-        'numero': 1,
-        'strike': false,
-        'spare': true,
-        'lances': [{
-          'numero': 1,
-          'quilles': 3
-        },
-        {
-          'numero': 2,
-          'quilles': '/'
-        }]
-      },
-      {
-        'numero': 2,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 1
-        },
-        {
-          'numero': 2,
-          'quilles': 2
-        }]
-      },
-      {
-        'numero': 3,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 9
-        },
-        {
-          'numero': 2,
-          'quilles': 0
-        }]
-      }]
-    }],
-  },
-  {
-    'title': 'Partie 2342',
-    'id': 1,
-    'piste': 2,
-    'team': [{
-      'name': 'Mireille Matthieu',
-      'position': 1,
-      'coups': [{
-        'numero': 1,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 5
-        },
-        {
-          'numero': 2,
-          'quilles': 3
-        }]
-      },
-      {
-        'numero': 2,
-        'strike': true,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 'X'
-        },
-        {
-          'numero': 2,
-          'quilles': '/'
-        }]
-      },
-      {
-        'numero': 3,
-        'strike': true,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 'X'
-        },
-        {
-          'numero': 2,
-          'quilles': '/'
-        }]
-      },
-      {
-        'numero': 4,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 6
-        },
-        {
-          'numero': 2,
-          'quilles': 2
-        }]
-      },
-      {
-        'numero': 5,
-        'strike': false,
-        'spare': true,
-        'lances': [{
-          'numero': 1,
-          'quilles': 6
-        },
-        {
-          'numero': 2,
-          'quilles': '/'
-        }]
-      },
-      {
-        'numero': 6,
-        'strike': true,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 'X'
-        },
-        {
-          'numero': 2,
-          'quilles': '/'
-        }]
-      },
-      {
-        'numero': 7,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 1
-        },
-        {
-          'numero': 2,
-          'quilles': 7
-        }]
-      },
-      {
-        'numero': 8,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 2
-        },
-        {
-          'numero': 2,
-          'quilles': 2
-        }]
-      },
-      {
-        'numero': 9,
-        'strike': false,
-        'spare': true,
-        'lances': [{
-          'numero': 1,
-          'quilles': 4
-        },
-        {
-          'numero': 2,
-          'quilles': '/'
-        }]
-      },
-      {
-        'numero': 10,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 2
-        },
-        {
-          'numero': 2,
-          'quilles': 3
-        }]
-      }]
-    },
-    {
-      'name': 'Jean Pierre Foucault',
-      'position': 2,
-      'coups': [{
-        'numero': 1,
-        'strike': false,
-        'spare': true,
-        'lances': [{
-          'numero': 1,
-          'quilles': 3
-        },
-        {
-          'numero': 2,
-          'quilles': '/'
-        }]
-      },
-      {
-        'numero': 2,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 1
-        },
-        {
-          'numero': 2,
-          'quilles': 2
-        }]
-      },
-      {
-        'numero': 3,
-        'strike': false,
-        'spare': false,
-        'lances': [{
-          'numero': 1,
-          'quilles': 9
-        },
-        {
-          'numero': 2,
-          'quilles': 0
-        }]
-      }]
-    }],
-  }]
-}
+var game;
+
+// EVENEMENT : Quand un client (une télé) se connecte au serveur
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('who_are_you')
+
+  // EVENEMENT : On demande au client quel télé est-elle (son ID)
+
+  socket.emit('who_are_you');
+
+  /*
+   * EVENEMENT : - On connais l'ID de la télé grace à DATA
+   *             - On lui envoient les informations de la partie qu'elle doit afficher
+   */
+
   socket.on('who_am_i', function (data) {
-    var active_game = false
-    for (index = 0; index < games.games.length; ++index) {
-      console.log(data.id + ' - ' + games.games[index].piste)
-      if (games.games[index].piste == data.id) {
-        active_game = games.games[index]
+    var url = 'http://bowling.noip.me/lanes/' + data.id + '/available';
+
+    console.log('---------------------------------------------------------------------------------------')
+    console.log('|   La TV numéro ' + data.id + ' à envoyer une requete à ' + url + '   |')
+    console.log('---------------------------------------------------------------------------------------')
+
+    // On envoient une requête HTTP au WebService pour savoir si il y à une partie active sur la piste
+
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var available_request = new XMLHttpRequest();
+    available_request.open('GET', url, true);
+
+    available_request.onload = function() {
+
+      if (available_request.status >= 200 && available_request.status < 400){
+        available = JSON.parse(available_request.responseText);
+
+        // Si il y a une partie
+
+        if (!available) {
+
+          var timer = setInterval(function() {
+          var url   = 'http://bowling.noip.me/lanes/' + data.id + '/game';
+            return true
+          }, 2000);
+
+          // On demandent au WebService de nous envoyer la partie en cours
+
+          var request = new XMLHttpRequest();
+          request.open('GET', url, true);
+
+          request.onload = function() {
+            if (request.status >= 200 && request.status < 400){
+              socket.emit('set_game', { game: JSON.parse(request.responseText) });
+            } else {
+              console.log('Impossible de joindre le serveur')
+              socket.emit('no_active_game', { game: false });
+            }
+          };
+          request.send();
+
+          // Si il n'y a pas de partie
+
+        } else {
+
+          // On dit à la télé qu'il n'y à pas de partie active
+
+          socket.emit('no_active_game', { game: false });
+        }
+
+      } else {
+
+        // Si le WebService ne répond pas, on dit à la télé qu'il n'y à pas de partie active
+
+        console.log('Impossible de joindre le serveur')
+        socket.emit('no_active_game', { game: false });
       }
-    }
-    if (active_game) {
-      socket.emit('set_game', { game: active_game });
-    } else {
-      socket.emit('no_active_game', { game: false });
-    }
-  })
+    };
+
+    available_request.send();
+  });
 });
--- public/javascripts/realtime.js
+++ public/javascripts/realtime.js
@@ -1,68 +1,59 @@
+// On se connecte au socket
+
 var socket = io.connect('http://localhost:3000');
 
+// EVENEMENT : Il y à une partie en cours
+
 socket.on('set_game', function (data) {
-  console.log(data)
   var scoretable = $('#scoretable')
 
-  _.each(data.game.team, function(player){
-    var line = '';
-    var total = 0;
-    var nb_coups = 10;
-    var is_strike = false;
-    var is_spare = false;
-    line += ('<tr><td>' + player.name + '</td>');
-    _.each(player.coups, function(lances){
+  // On boucle sur les PLAYERS de la partie en cours
+
+  _.each(data.game.players, function(player){
+    var final_score = 0;
+    var nb_coups    = 10;
+    var line        = '';
+    line            += ('<tr><td>' + player.Pseudo + '</td>');
+
+    // On boucle sur chaque TOURS du PLAYER
+
+    _.each(player.turns, function(turn){
+      line += ('<td class="m-scoretable--td" id="' + player.Id + '-' + turn.Id + '">');
       nb_coups--;
-      line += ('<td class="m-scoretable--td">');
-      _.each(lances, function(quilles){
-        _.each(quilles, function(quille){
-          if(!lances.strike && !lances.spare) {
-            if(is_spare) {
-              total += (quille.quilles * 2);
-              is_spare = false;
-            } else {
-              if(is_strike){
-                total += (quille.quilles * 2);
-              } else {
-                total += quille.quilles;
-              }
-            }
-          }
-          line += ('<div class="m-scoretable--col text-center">' + quille.quilles + '</div> ');
-        })
+
+      // On boucle sur chaque LANCES du TOUR
+
+      _.each(turn.throws, function(throww){
+        line += ('<div class="m-scoretable--col text-center">' + throww.Fallen_skittles + '</div>');
       })
-      if(lances.strike) {
-        if(is_strike || is_spare){
-          total += 20;
-          is_spare = false;
-        } else {
-          total += 10;
-        }
-        is_strike = true;
-      } else if(lances.spare) {
-        if(is_strike){
-          total += 20;
-        } else {
-          total += 10;
-        }
-        is_spare = true;
-        is_strike = false;
-      } else {
-        is_spare = false;
-        is_strike = false;
-      }
-      line += ('</br><div class="m-scoretable--col--score text-right">' + total + '</div>');
-      line += ('</td>');
+
+      line += ('</br><div class="m-scoretable--col--score text-right">' + turn.Score + '</div></td>');
+      final_score = turn.Score
     })
+
+    // On remplie de cases vides les coups pas encore joués
+
     for (var i=0;i<nb_coups;i++) {
       line += ('<td></td>');
     }
-    line += ('<td><b>' + total + '</b></td>');
+
+    // On affiche le score final
+
+    if (final_score == undefined) {
+      line += ('<td><b>0</b></td>');
+    } else {
+      line += ('<td><b>' + final_score + '</b></td>');
+    }
+
     line += ('</tr>');
+
+    // On ajoute le tour au tableau des scores
 
     scoretable.append(line);
   })
 });
+
+// EVENEMENT : Pas de partie en cours
 
 socket.on('no_active_game', function (data) {
   $('#container-score').html('<div class="container"><div class="row"><div class="col-lg-12 text-center"><h1>Aucune partie active sur cette piste</h1></div></div></div>')
